8ee0c2d16e817a191af7f35e52a075f01e7fe963
Jedidiah John <jedi.unicef@gmail.com>
2021-06-24 18:27:38 -0400

    Feature: Alert, Add, Input from user browser with try and catch

--- demo/js/print.js
+++ demo/js/print.js
@@ -1,2 +1,21 @@
 console.log("this is console");
-alert("this is alert");
+// alert("this is alert");
+
+//add 2 nos
+console.log("sum" + " " + (4 +3));
+
+// add 2 input from browser user
+try {
+    let num1 = parseInt(prompt("type first number"));
+    let num2 = parseInt(prompt("type second number"));
+    let num3 = parseInt(prompt("type third number"));
+    let num4 = num1 * num2 / num3;
+    alert(num4);
+
+}
+
+catch (e){
+    console.log("Run in Browser", e);
+}
+
+
