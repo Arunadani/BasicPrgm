93bbefba8c05442000e785664b60066ddadbaad5
Jedidiah John <jedi.unicef@gmail.com>
2021-06-25 11:11:45 -0400

    Feature: check numbers were odd or even

---
+++ demo/js/oddeven.js
@@ -0,0 +1,16 @@
+//check odd or even
+//get input
+const num = prompt('Enter a number: ');
+//const num = 65341;
+// check if even
+if(num % 2 === 0) {
+    console.log("The number is even.");
+}
+// odd
+else console.log("The number is odd");
+
+//ternary operator
+// const result = (number % 2  == 0) ? "even" : "odd";
+//
+//display the result
+// console.log(`The number is ${result}.`);
