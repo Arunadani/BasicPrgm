b15404b46339d846bb370a8a156850c852d30e99
Jedidiah John <jedi.unicef@gmail.com>
2021-06-25 11:04:18 -0400

    Feature: check numbers were positive, negative or zero

---
+++ demo/js/checknos1.js
@@ -0,0 +1,28 @@
+// check num's +ve, -ve or 0
+// get input
+const num = parseInt(prompt('Enter a number: '));
+//const num = 1;
+//check num is greater than zero
+if (num > 0){
+    console.log("The number is positive");
+}
+// check num is equal to zero
+else if (num === 0){
+   console.log("The number is zero");
+}
+else console.log("The number is negative");
+
+// Using while
+/* const number = prompt("Enter a number: ");
+
+if (number >= 0) {
+    if (number == 0) {
+        console.log("The number is zero");
+    } else {
+        console.log("The number is positive");
+    }
+} else {
+    console.log("The number is negative");
+} */
+
+
