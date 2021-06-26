71c56347733a922c8d38676381b2088e3ad0b572
Jedidiah John <jedi.unicef@gmail.com>
2021-06-26 18:41:01 -0400

    Feature: array

--- demo/js/array_cars.js
+++ demo/js/array_cars.js
@@ -5,5 +5,39 @@ cars[2] = "Tesla";
 cars[3] = "Royce Rolls";
 cars[4] = "Benz";
 cars[5] = "volvo";
-console.log(cars);
-console.log(cars.length)
+const values = [5,6,3,2,1,0,9,8,7,4,3,1,3,5,2,4];
+const newval = values.map(newfunction);
+//let text = "";
+//console.log(cars);
+//console.log(cars.length);
+//sort
+//console.log(cars.sort());
+//console.log(cars.reverse());
+//numeric sort
+
+//console.log(values.sort(function(a, b){return a - b}));
+//console.log(values.sort(function(a, b){return b - a}));
+
+/*myfunction(values);
+
+function myfunction(myArray){
+    myArray.sort(function (a,b){ return a - b});
+    console.log(myArray);
+}*/
+
+/*values.forEach(myfunc);
+function myfunc(array, value, index){
+     console.log(array,value, index);
+}*/
+
+function newfunction(valuese){
+            return valuese*20;
+
+}
+console.log(values);
+console.log(newval);
+
+
+
+
+
