fd601128f7774d1910262b480c9df52415eb26ff
Jedidiah John <jedi.unicef@gmail.com>
2021-06-27 19:17:46 -0400

    feature: arr copy mul 2

--- demo/js/copytosecondarray.js
+++ demo/js/copytosecondarray.js
@@ -3,6 +3,7 @@ let num2 = [];
 n = num1.length
 
 for (let i = 0; i<n; i++){
-    num2[i] = num1[i];
+    // num1[i] = num1[i] * 2;
+    num2[i] = num1[i] * 2;
 }
-console.log("The values of num 2 is: " + num2);
+console.log("The values of num 2 * 2 is: " + num2);
