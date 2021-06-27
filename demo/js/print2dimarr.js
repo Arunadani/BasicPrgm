21e14bddbd08efe720dff59a96109d3692e13802
Jedidiah John <jedi.unicef@gmail.com>
2021-06-27 19:49:03 -0400

    feature: 2d array print

---
+++ demo/js/print2dimarr.js
@@ -0,0 +1,14 @@
+let num = [ [1,3,4],
+            [3,4,2],
+            [1,4,3],
+            [7,3,2],
+            [8,4,3]]
+let n = num.length;
+for (let i = 0; i<5; i++){
+    for (let j=0; j<3; j++){
+        //console.log(`arrays are ${num[i]}`);
+
+    }
+    console.log(`arrays are ${num[i]}`);
+}
+
