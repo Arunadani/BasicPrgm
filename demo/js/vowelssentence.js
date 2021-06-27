3696bbc18ba288aacba4d75f848e0f312a5d77fb
Jedidiah John <jedi.unicef@gmail.com>
2021-06-26 20:07:30 -0400

    Feature: vowel sentence

---
+++ demo/js/vowelssentence.js
@@ -0,0 +1,25 @@
+//let text = "Whoever dwells in the shelter of the Most High will rest in the shadow of the Almighty"
+let text = "mdm is here"
+let vowelsArr = [];
+let words;
+    wordcount(text);
+
+function wordcount(counts) {
+    console.log(counts.trim());
+    words = counts.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        for (j = 0; j <= words[i].length; j++){
+            if (words[i][j] == "a" || words[i][j] == "e" || words[i][j] == "i" || words[i][j] == "o" || words[i][j] == "u") {
+                vowelsArr.push(words[i]);
+                break;
+            }
+        }
+    }
+}
+console.log("total words count: " + words.length);
+
+console.log(vowelsArr);
+
+console.log('Count: '+ vowelsArr.length);
+
