69bf76423779989e3459fdc9e56d7a97cd97f516
Jedidiah John <jedi.unicef@gmail.com>
2021-06-28 15:15:16 -0400

    feature: java call two Methods with object

---
+++ demo/src/main/java/Twoclassobj_main.java
@@ -0,0 +1,7 @@
+public class Twoclassobj_main {
+    public static void main (String[] args){
+        Twoclassobj_method height = new Twoclassobj_method();
+        height.aeroplane();
+        height.distance(500);
+    }
+}
---
+++ demo/src/main/java/Twoclassobj_method.java
@@ -0,0 +1,9 @@
+public class Twoclassobj_method {
+    public void aeroplane(){
+        System.out.println("The aeroplane is flying high");
+    }
+    public void distance(int height){
+        System.out.println("Height is: " + height);
+    }
+
+}
