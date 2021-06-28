6d4298f47b0a31d6d734fe9ce29d4f1af003c718
Jedidiah John <jedi.unicef@gmail.com>
2021-06-28 14:51:43 -0400

    feature: java call Methods

---
+++ demo/src/main/java/CallMethods.java
@@ -0,0 +1,12 @@
+public class CallMethods {
+    static void myFunction(){
+        int i = 2;
+        int j = 12;
+        int k = 21;
+        System.out.println("The value is:" + ((i*k)/j));
+    }
+    public static void main(String[] args){
+        myFunction();
+    }
+
+}
