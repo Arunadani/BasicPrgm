6aa8a4ca436919dc9678556b41debad1163adee4
Jedidiah John <jedi.unicef@gmail.com>
2021-06-29 21:51:46 -0400

    feature: sum each digit yet to

---
+++ demo/js/sumeachdigits.js
@@ -0,0 +1,16 @@
+let num  = 172;
+
+if (isNaN){
+    console.log("Please enter valid number.");
+    console.log(num);
+}
+else do {
+    let temp = num % 10;
+   n = num/10;
+    sum+= temp;
+    console.log(temp);
+}while (n<=0){
+
+}
+
+
