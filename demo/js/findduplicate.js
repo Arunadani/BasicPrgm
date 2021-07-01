6fe5d595bfd02a20441efb41ea4c63c443662858
Jedidiah John <jedi.unicef@gmail.com>
2021-07-01 19:55:05 -0400

    feature: find duplicates

--- demo/js/findduplicate.js
+++ demo/js/findduplicate.js
@@ -10,12 +10,44 @@ function duplicate(randomnum){
             counts[randomnum[i]] = 1;
         }
     }
-    for (let j in counts){
-        if (counts[j] >= 2){
-            console.log(j + " duplicated: " + counts[j] + " times.");
+    for (let i in counts){
+        if (counts[i] >= 2){
+            console.log(i + " duplicated: " + counts[i] + " times.");
         }
     }
     return counts;
 }
 
 
+
+
+let arr = ["abc","xy","bb", "abc"];
+let chk = checkDuplicate(arr);
+console.log(chk);
+function checkDuplicate(arr) {
+    // call some function with callback function as argument
+    for(let i = 0; i < arr.length;i++) {
+        for(let j = 0; j < arr.length;j++) {
+            // do not compare same elements
+            if(i !== j) {
+                // check if elements match
+                if(arr[i] === arr[j]){
+                    // duplicate element found
+                    result = true;
+                    // terminate inner loop
+                    break;
+                }
+            }
+        }
+        // terminate outer loop
+        if(result){
+            break;
+        }
+    }
+    if(result) {
+        console.log('Array contains duplicate elements');
+    } else {
+        console.log('Array does not contain duplicate elements');
+    }
+    return result;
+}
