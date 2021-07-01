3814f71885f7efa693224fd3b28e7908f67187fa
Jedidiah John <jedi.unicef@gmail.com>
2021-07-01 19:55:41 -0400

    feature: reverse number

--- demo/js/reversenumber.js
+++ demo/js/reversenumber.js
@@ -1,12 +1,14 @@
-let num = 63165432;
-let reversenum =reverseint(num);
-console.log(reversenum);
+{
+    const num = 465;
+    let reversenum = reverseint(num);
+    console.log(reversenum);
+}
 function reverseint(reversenum) {
     let result = 0, counter = 0;
     while(reversenum){
         result = reversenum % 10;
         counter = (counter * 10) + result;
-        reversenum = reversenum / 10 | 0;
+        reversenum = parseInt(reversenum / 10);
     }
     return counter;
 }
