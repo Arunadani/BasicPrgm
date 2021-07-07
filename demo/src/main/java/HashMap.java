ca5b91d9788754161d1aa7cd5e3d8558f9aff69f
Jedidiah John <jedi.unicef@gmail.com>
2021-07-07 12:45:51 -0400

    feature: Java hashmap

---
+++ demo/src/main/java/HashMap.java
@@ -0,0 +1,10 @@
+import.java.util.HashMap;
+int a = 10;
+int b = 5;
+int c = 82;
+
+HashMap<String, int> integer = new HashMap<String, int>();
+
+integer.put("a", 10);
+integer.put("b", 5);
+integer.put("c", 82);
