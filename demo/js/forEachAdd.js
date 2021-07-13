c555e9b4211d97a9adf2dfeda734ca3446896594
Jedidiah John <jedi.unicef@gmail.com>
2021-07-12 22:25:10 -0400

    feature: foreach add

---
+++ demo/js/forEachAdd.js
@@ -0,0 +1,17 @@
+let num = [2,4,1];
+let sum =0;
+
+num.forEach(numb);
+function numb(item,value,arr)
+{
+    //console.log(value);
+    //console.log(item);
+
+    sum = sum + arr[value];
+
+    //console.log(arr);
+    //console.log(num);
+  //  sum = sum + num[sum];
+    }
+
+console.log(sum);
