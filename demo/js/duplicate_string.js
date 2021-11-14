4ec56c768ed45a73cb2b94b69c22f9ded67d5f59
Jedidiah John <jedi.unicef@gmail.com>
2021-11-13 22:54:02 -0500

    string duplicate

---
+++ demo/js/duplicate_string.js
@@ -0,0 +1,28 @@
+let str = "Stringmaycontainsomething";
+
+let charlen = 256;
+
+findDuplicate(str);
+
+console.log(duplicate);
+
+function findDuplicate(newStr){
+    
+    let count = new Array(charlen);
+    count.fill(0);
+    
+    for (let i = 0; i < newStr.length; i++)
+            count[newStr[i].charCodeAt()]++;
+             
+        for (let i = 0; i < charlen; i++)
+        {
+            if(count[i] > 1)
+            {
+                console.log(String.fromCharCode(i) +
+                count[i]);
+            }
+
+            }
+            //return str;
+
+}
